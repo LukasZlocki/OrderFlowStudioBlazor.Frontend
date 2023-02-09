a961b09a4fe3a7d926aecc514f7a2da772e12463
lzlocki <lzlocki@poczta.fm>
2023-02-09 21:32:40 +0100

    Add bracket - fix bug

--- OrderFlowStudio.Frontend/Data/Raport.cs
+++ OrderFlowStudio.Frontend/Data/Raport.cs
@@ -6,4 +6,5 @@ namespace OrderFlowStudio.Frontend.Data
         public int QuantityFinished { get; set; }
         public int OrderId { get; set; }
         public ProductionStatus Status { get; set; }
+    }
 }
