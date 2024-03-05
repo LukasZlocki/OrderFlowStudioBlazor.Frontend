2a8d4b855373c336d224e04ad2b1d4ff4745735b
lzlocki <lzlocki@poczta.fm>
2024-03-05 21:28:36 +0100

    Fix listing of all orders

--- OrderFlowStudio.Frontend/Data/Order.cs
+++ OrderFlowStudio.Frontend/Data/Order.cs
@@ -6,7 +6,7 @@ namespace OrderFlowStudio.Frontend.Data
         public int OrderNumber { get; set; } 
         public int Quantity { get; set; }
         public Product productDto{ get; set; }
-        public Raport raportDto {get; set;}
+        public Report reportDto {get; set;}
     }
 }
 
